f7176faf052d624fddbaefb690fb23b059fb3bd5
Eostream <cossumika@gmail.com>
2020-09-19 20:42:13 +0200

    TCamera base script

---
+++ Assets/Functionnal/Scripts/Behaviours/TCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+namespace InterviewTask
+{
+	public class TCamera : MonoBehaviour
+	{
+		[SerializeField] private TEntity _player;
+	}
+}
