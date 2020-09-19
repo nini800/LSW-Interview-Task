4eba1f992903092768f852e419289c60f74e7887
Eostream <cossumika@gmail.com>
2020-09-20 00:23:14 +0200

    Visual importation of shop and bacground visual

---
+++ Assets/Functionnal/Scripts/World/ShopDoors.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InterviewTask
+{
+	public class ShopDoors : MonoBehaviour
+	{
+		[Header("References")]
+		[Space]
+		[SerializeField] private SpriteRenderer _spriteRenderer;
+		[SerializeField] private Sprite _openSprite;
+		[SerializeField] private Sprite _closeSprite;
+
+		private void OnTriggerEnter2D(Collider2D collision)
+		{
+			if (collision.gameObject.GetComponentInParent<TEntity>())
+			{
+				_spriteRenderer.sprite = _openSprite;
+			}
+		}
+
+		private void OnTriggerExit2D(Collider2D collision)
+		{
+			if (collision.gameObject.GetComponentInParent<TEntity>())
+			{
+				_spriteRenderer.sprite = _closeSprite;
+			}
+		}
+	}
+}
