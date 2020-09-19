a1da7066171ae4c74f4592c503175cffbf8e4570
Eostream <cossumika@gmail.com>
2020-09-19 22:19:21 +0200

    Camera Move System Base

--- Assets/Functionnal/Scripts/Behaviours/TCamera.cs
+++ Assets/Functionnal/Scripts/Behaviours/TCamera.cs
@@ -10,16 +10,24 @@ namespace InterviewTask
 		[Header("Parameters")]
 		[Space]
 		[SerializeField, Min(0f)] private float _cameraSmoothTime = 0.1f;
+		[SerializeField] private Vector2 _cameraZoomLimits = new Vector2(2.5f, 10f);
 
 		[Header("References")]
 		[Space]
 		[SerializeField] private TEntity _player;
+		[SerializeField] private Camera _camera;
 
+		private float _currentZoom;
 		private Vector3 _velocity;
 
+		private void Awake()
+		{
+			_currentZoom = Mathf.Sqrt(_camera.orthographicSize);
+		}
 		private void Update()
 		{
 			HandleFollowPlayer();
+			HandleZoom();
 		}
 		private void HandleFollowPlayer()
 		{
@@ -30,6 +38,15 @@ namespace InterviewTask
 				_player.Center.ToV3(transform.position.z),
 				ref _velocity,
 				_cameraSmoothTime);
+		}
+		private void HandleZoom()
+		{
+			float input = Input.GetAxis("Zoom");
+			_currentZoom += input;
+			_currentZoom = Mathf.Clamp(_currentZoom, _cameraZoomLimits.x, _cameraZoomLimits.y);
+
+			//Real orthographicSize is squared to have a linear feeling when zooming in and out
+			_camera.orthographicSize = _currentZoom * _currentZoom;
 		}
 	}
 }
